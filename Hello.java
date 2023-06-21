f8aaedffd49ce65480411562b7d60765536a9be6
jb <101614021+jbc2313@users.noreply.github.com>
2023-06-21 12:23:40 -0500

    init commit

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    // File name has to be class name: ie.. Hello
+
+    // main is where the program runs
+
+    public static void main(String []args) {
+        System.out.println("Hello World, This is Java running on OpenJDK");
+
+
+        Username user1 = new Username();
+        user1.Hi();
+    }
+
+
+
+}
---
+++ Username.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Username {
+    public String Uname;
+    Scanner userObj = new Scanner(System.in);
+
+    public Username() {
+       System.out.println("Enter your username:");
+       String username = userObj.nextLine();
+       Uname = username;
+    }
+
+
+    public void Hi() {
+        System.out.println(this.Uname);
+
+    }
+}
